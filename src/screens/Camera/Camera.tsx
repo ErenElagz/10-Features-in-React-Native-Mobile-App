8c33e55d42164f3c70317e3aad377c1e58635a69
ErenElagz <erenelagoz@gmail.com>
2025-04-09 13:01:05 +0200

    feat: Add camera functionality and permissions handling in QR and Camera screens; update dependencies in package.json and AndroidManifest.xml

--- src/screens/Camera/Camera.tsx
+++ src/screens/Camera/Camera.tsx
@@ -1,14 +1,65 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Alert, Platform, Text} from 'react-native';
+import React, {useEffect} from 'react';
 import Pages from '../../data/Pages';
+import {Camera, useCameraDevice} from 'react-native-vision-camera';
+import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import {useNavigation} from '@react-navigation/native';
 
-export default function CameraScreen() {
+export default function QrScreen() {
+  const checkCameraPermission = async () => {
+    const result = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.CAMERA
+        : PERMISSIONS.ANDROID.CAMERA,
+    );
+
+    switch (result) {
+      case RESULTS.GRANTED:
+        break;
+      case RESULTS.DENIED:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is required to scan QR codes.',
+        );
+        break;
+      case RESULTS.BLOCKED:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is blocked. Please enable it from settings.',
+        );
+        break;
+      default:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is required to scan QR codes.',
+        );
+    }
+  };
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  const nav = useNavigation();
+  const devices = useCameraDevice('back');
+
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View>
         <Text style={styles.function}>Function {Pages[1].index}</Text>
         <Text style={styles.title}>{Pages[1].title}</Text>
         <Text style={styles.description}>{Pages[1].description}</Text>
+      </View>
+      <View
+        style={{
+          width: '100%',
+          height: 400,
+          marginTop: 16,
+          borderRadius: 32,
+          overflow: 'hidden',
+        }}>
+        {devices && (
+          <Camera style={styles.camera} device={devices} isActive={true} />
+        )}
       </View>
     </View>
   );
@@ -34,6 +85,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: '#234897',
     letterSpacing: -0.5,
+    fontWeight: 'bold',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  qrCodeText: {
+    fontSize: 24,
+    color: '#000',
+    marginTop: 16,
+    textAlign: 'center',
     fontWeight: 'bold',
   },
 });
--- src/screens/QR/QR.tsx
+++ src/screens/QR/QR.tsx
@@ -1,14 +1,86 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {View, StyleSheet, Alert, Platform, Text} from 'react-native';
+import React, {useEffect} from 'react';
 import Pages from '../../data/Pages';
+import {
+  Camera,
+  useCameraDevice,
+  useCodeScanner,
+} from 'react-native-vision-camera';
+import {request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 export default function QrScreen() {
+  const [qrCode, setQrCode] = React.useState('Scan a QR code');
+
+  const checkCameraPermission = async () => {
+    const result = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.CAMERA
+        : PERMISSIONS.ANDROID.CAMERA,
+    );
+
+    switch (result) {
+      case RESULTS.GRANTED:
+        break;
+      case RESULTS.DENIED:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is required to scan QR codes.',
+        );
+        break;
+      case RESULTS.BLOCKED:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is blocked. Please enable it from settings.',
+        );
+        break;
+      default:
+        Alert.alert(
+          'Camera Permission',
+          'Camera permission is required to scan QR codes.',
+        );
+    }
+  };
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  const devices = useCameraDevice('back');
+  const codeScanner = useCodeScanner({
+    codeTypes: ['qr', 'ean-13'],
+    onCodeScanned: codes => {
+      if (codes.length > 0) {
+        const code = codes[0];
+        setQrCode(code.value || '');
+      }
+    },
+  });
+
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <View>
         <Text style={styles.function}>Function {Pages[0].index}</Text>
         <Text style={styles.title}>{Pages[0].title}</Text>
         <Text style={styles.description}>{Pages[0].description}</Text>
+      </View>
+      <View
+        style={{
+          width: '100%',
+          height: 400,
+          marginTop: 16,
+          borderRadius: 32,
+          overflow: 'hidden',
+        }}>
+        {devices && (
+          <Camera
+            style={styles.camera}
+            device={devices}
+            isActive={true}
+            codeScanner={codeScanner}
+          />
+        )}
+      </View>
+      <View>
+        <Text style={styles.qrCodeText}>{qrCode}</Text>
       </View>
     </View>
   );
@@ -35,5 +107,18 @@ const styles = StyleSheet.create({
     color: '#234897',
     letterSpacing: -0.5,
     fontWeight: 'bold',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  qrCodeText: {
+    fontSize: 24,
+    color: '#000',
+    marginTop: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    letterSpacing: -1,
   },
 });
